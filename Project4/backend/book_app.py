34d5a9cc3ad9c9649acdc579565d0735bebaa2ab
R <aparna.r03@sap.com>
2022-11-28 17:13:41 +0530

    code push

---
+++ Project4/backend/book_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, abort
+import database as database
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.get('/all')
+def get_all_books():
+    return database.get_all_data()
+
+@app.get('/search')
+def get_results():
+    name = request.args.get('name', '')
+    author = request.args.get('author', '')
+    if name == '' and author == '':
+        abort(400)
+    return database.get_query_result(name,author)
+
+@app.get('/book/<book_id>')
+def get_book(book_id):
+    result= database.get_book_data(book_id)
+    if result == None:
+        abort(404)
+    return result
+
---
+++ Project4/backend/database.py
@@ -0,0 +1,79 @@
+import os
+import psycopg2
+
+#PostgreSql config
+#import pdb;pdb.set_trace()
+conn = psycopg2.connect(
+    dbname="book_library",
+    user="postgres",
+    password="admin",
+    host="localhost",
+    port=5432
+)
+#conn = psycopg2.connect(host=os.environ.get('POSTGRES_HOST'),
+#   port=os.environ.get('POSTGRES_PORT'),
+#    user=os.environ.get('POSTGRES_USER'),
+#   password=os.environ.get('POSTGRES_PASS'),
+#   dbname=os.environ.get('POSTGRES_DB'))
+    
+cursor = conn.cursor()
+
+def get_all_data():
+    Query = "select * from books_data;"
+    cursor.execute(Query)
+    record_new= cursor.fetchall()
+    results_new=[]
+    for row in record_new:
+        results_new.append(
+            {
+                "name": row[0],
+                "image":row[1],
+                "category":row[2],
+                "author":row[3],
+                "format":row[4],
+                "price":row[5],
+                "isbn":row[6]
+            }
+        )
+    return results_new
+
+def get_query_result(name, author):
+    if name != '':
+        Query = "select * from books_data where name ILIKE '%s'" % name
+    if author != '':
+        Query = "select * from books_data where author ILIKE '%s'" % author
+    if name != '' and author != '':
+        Query = "select * from books_data where name ILIKE '%s' and author ILIKE '%s'" % (name,author)
+    cursor.execute(Query)
+    records= cursor.fetchall()
+    results=[]
+    for row in records:
+        results.append(
+            {
+                "name": row[0],
+                "image":row[1],
+                "category":row[2],
+                "author":row[3],
+                "format":row[4],
+                "price":row[5],
+                "isbn":row[6]
+            }
+        )
+    return results
+
+def get_book_data(book_id):
+    Query = "select * from books_data WHERE isbn = %s" % book_id
+    cursor.execute(Query)
+    table=cursor.fetchall()
+    if len(table) == 0:
+        return None
+    book_data=table[0]
+    return {
+        "name": book_data[0],
+        "image":book_data[1],
+        "category":book_data[2],
+        "author":book_data[3],
+        "format":book_data[4],
+        "price":book_data[5],
+        "isbn":book_data[6]
+    }
