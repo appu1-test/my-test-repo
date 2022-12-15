5257c647b4ee972e300d2f3baebc64fed1d20ea5
R <aparna.r03@sap.com>
2022-12-15 23:29:58 +0530

    add book frontend

---
+++ Project4/book_frontend_2/src/App.js
@@ -0,0 +1,89 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react"; 
+import SearchForm from './SearchForm'
+let table=undefined
+class App extends React.Component {
+  clickme(that){
+    fetch("http://localhost:5000/")
+    .then(response => {
+      if (!response.ok){
+        alert ("Could not fetch data") 
+      }
+      else {
+        return response.json()
+      }
+    })
+    .then(data => {
+      console.log(data)
+      table= (
+      <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Author</th>
+          <th>Category</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((e) => {
+          return (<tr>
+            <td>{e.name}</td>
+            <td>{e.author}</td>
+            <td>{e.category}</td>
+            <td><img src = {e.image} alt= {e.name}></img></td>
+          </tr>)
+        })}
+      </tbody>
+      </table>
+     )
+     console.log(that)
+    //  document.getElementById("search_data").innerHTML = "";
+     that.forceUpdate()
+    //  let div= document.getElementById('data')
+    //  div.appendChild(React.createElement(table))
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  
+  render() {
+    var tableStyle = {
+       "border": "1px solid black",
+       "borderCollapse": "collapse"
+    }
+    return(
+      <div>
+        <style>{`
+          table,th,td{
+           border: 1px solid black;
+           border-collapse: collapse;
+          }
+        `}</style>
+        <h1>Book Library</h1>
+        <button onClick={() => this.clickme(this)}>All Books</button>
+        <SearchForm></SearchForm>
+        <div id = 'data'>
+          {table}
+        </div>
+      </div>  
+    );
+  }
+}
+
+// const TableHeader = () => { ... }
+// const TableBody = () => { ... }
+// class Table extends Component {
+//   render() {
+//     return (
+//       <table>
+//         <TableHeader />
+//         <TableBody />
+//       </table>
+//     )
+//   }
+// }
+
+export default App;
---
+++ Project4/book_frontend_2/src/SearchForm.js
@@ -0,0 +1,92 @@
+import React from "react";
+let table = undefined
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: '',
+                      author: ''};
+
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeAuthor = this.handleChangeAuthor.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeName(event) {
+        this.setState({name: event.target.value});
+    }
+    handleChangeAuthor(event) {
+        this.setState({author: event.target.value});
+    }
+
+
+    handleSubmit(event) {
+        console.log(this)
+        fetch("http://localhost:5000/search?" + new URLSearchParams({
+            name: this.state.name,
+            author: this.state.author
+        }))
+        .then(response => {
+            if (!response.ok){
+            alert ("Could not fetch data") 
+            }
+            else {
+            return response.json()
+            }
+        })
+        .then(data => {
+            console.log(data)
+            table= (
+            <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Author</th>
+                <th>Category</th>
+                <th>Image</th>
+            </tr>
+            </thead>
+            <tbody>
+            {data.map((e) => {
+                return (<tr>
+                <td>{e.name}</td>
+                <td>{e.author}</td>
+                <td>{e.category}</td>
+                <td><img src = {e.image} alt= {e.name}></img></td>
+                </tr>)
+            })}
+            </tbody>
+            </table>
+            )
+            console.log(this)
+            // document.getElementById("data").innerHTML = "";
+             this.forceUpdate()
+        //  let div= document.getElementById('data')
+        //  div.appendChild(React.createElement(table))
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+                </label>
+                <label>
+                    Author:
+                    <input type="text" value={this.state.author} onChange={this.handleChangeAuthor} />
+                </label>
+                <input type="submit" value="Search" />
+            </form>
+            <div id = "search_data"> {table} </div>
+            </div>
+        );
+    }
+}
+
+export default SearchForm;
