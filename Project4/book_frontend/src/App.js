f03dba602f443f1db6f54ff4283de52c839195cd
R <aparna.r03@sap.com>
2023-01-02 10:27:17 +0530

    changes to ip frontend

--- Project4/book_frontend/src/App.js
+++ Project4/book_frontend/src/App.js
@@ -5,7 +5,7 @@ import SearchForm from './SearchForm'
 let table=undefined
 class App extends React.Component {
   clickme(that){
-    fetch("http://ec2-3-74-231-77.eu-central-1.compute.amazonaws.com:5000")
+    fetch("ec2-3-69-151-96.eu-central-1.compute.amazonaws.com:5000")
     .then(response => {
       if (!response.ok){
         alert ("Could not fetch data") 
