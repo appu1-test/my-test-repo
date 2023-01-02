262567347025a74e64fd2446235e177d725fbd5d
R <aparna.r03@sap.com>
2023-01-02 11:00:52 +0530

    new changes

--- Project4/book_frontend/src/SearchForm.js
+++ Project4/book_frontend/src/SearchForm.js
@@ -28,7 +28,7 @@ class SearchForm extends React.Component {
 
     handleSubmit(event) {
         console.log(this)
-        fetch("ec2-3-69-151-96.eu-central-1.compute.amazonaws.com:5000/search?" + new URLSearchParams({
+        fetch("http://ec2-3-69-151-96.eu-central-1.compute.amazonaws.com:5000/search?" + new URLSearchParams({
             name: this.state.name,
             author: this.state.author
         }))
